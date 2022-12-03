ffb1b659d467fe8a083da1dd726df0dec31cd04e
OrelAlon <orela231089@gmail.com>
2022-12-03 14:17:56 +0200

    creating router to all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import { useState } from "react";
-import "./App.css";
+import { Container } from "react-bootstrap";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import NewNote from "./components/NewNote";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className='App'>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <p>React </p>
-      </div>
-    </div>
+    <Container className='my-4'>
+      <Routes>
+        <Route path='/' element={<NewNote />}></Route>
+        <Route path='/new' element={<h1>New</h1>}></Route>
+
+        <Route path='/:id'>
+          <Route index element={<h1>Show</h1>}></Route>
+          <Route path='edit' element={<h1>New</h1>}></Route>
+        </Route>
+        <Route path='*' element={<Navigate to='/' />}></Route>
+      </Routes>
+    </Container>
   );
 }
 
